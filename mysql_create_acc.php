4d4977cbde205d6d6f9c485e8c95875cfd0e9066
PREDAtorul <74401212+PREDAtorul@users.noreply.github.com>
2022-06-02 15:16:03 +0300

    upsie

--- mysql_create_acc.php
+++ mysql_create_acc.php
@@ -3,7 +3,7 @@
 $conn=mysqli_connect("localhost", "root", "") or die(mysqli_error());
 mysqli_select_db($conn, "db_users");
 
-$sql_create = "CREATE TABLE user
+$sql_create = "CREATE TABLE users
 (
 username varchar(20),
 password varchar(25),
